b9ab29d45e591e909e7956ba79222f7f17e06b9b
OggieBoggie <tommy.nguyen.vanc@gmail.com>
2022-11-23 14:17:35 -0800

    Amoung us color

--- script/script.js
+++ script/script.js
@@ -1,16 +1,14 @@
 const clickMeButton = document.querySelector(".click-me");
 // console.log(clickMeButton); #checks if code is working 
 
-function clickHandler ()
-{
+function clickHandler() {
     alert("You clicked me!");
     // clickMeButton.removeEventListener('click', clickHandler);
 }
 
-clickMeButton.addEventListener('click', clickHandler, {once:true});
+clickMeButton.addEventListener('click', clickHandler, { once: true });
 
-function pinkbackground (color)
-{
+function pinkbackground(color) {
     // document.body.style.background = color
     document.body.classList.add("pink")
 }
@@ -18,29 +16,58 @@ function pinkbackground (color)
 clickMeButton.addEventListener('click', pinkbackground)
 // clickMeButton.addEventListener('click', function() {pinkbackground('pink')});
 
-function changeText() 
+function changeText()
 // USE FOR DARK AND LIGHT THEME
 {
-    if(clickMeButton.textContent === "Click me!")
-    {
+    if (clickMeButton.textContent === "Click me!") {
         clickMeButton.textContent = "Clicked"
     }
-    else if (clickMeButton.textContent === "Clicked")
-    {
+    else if (clickMeButton.textContent === "Clicked") {
         clickMeButton.textContent = "Click me!"
     }
-    
+
 }
 clickMeButton.addEventListener("click", changeText)
 
-function updateImage(event)
-{
+function updateImage(event) {
     console.log(event)
     const image = document.querySelector('#shoppingCart')
     image.src = "images/263142.png";
-    image.width=100;
-    image.height=100;
-    image.alt= "Shopping cart"
+    image.width = 100;
+    image.height = 100;
+    image.alt = "Shopping cart"
 }
 
 clickMeButton.addEventListener("click", updateImage)
+
+const buttonContainer = document.querySelector(".button-container");
+
+function changeBGGreen(e) {
+    if (e.target.tagName === "BUTTON") {
+        e.target.classList.add("green")
+    }
+}
+
+buttonContainer.addEventListener("mouseover", changeBGGreen)
+
+function changeTextColor (e)
+{
+   if (e.target.textContent === 'red')
+   {
+        e.target.classList.add("red")
+   }
+   else if (e.target.textContent === "blue")
+   {
+        e.target.classList.add("blue")
+   }
+   else if (e.target.textContent === "white")
+   {
+        e.target.classList.add("white")
+   }
+   else if (e.target.textContent === "yellow")
+   {
+        e.target.classList.add("yellow")
+   }
+}
+
+buttonContainer.addEventListener("click", changeTextColor)
